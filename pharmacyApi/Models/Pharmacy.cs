3e3241700afa12df982cc397dac0d3e9ab647ec5
Sergula <post.gargalik@gmail.com>
2023-06-12 21:50:12 +0300

    Создание модели Pharmacy

--- pharmacyApi/Models/Pharmacy.cs
+++ pharmacyApi/Models/Pharmacy.cs
@@ -1,4 +1,12 @@
 ﻿namespace pharmacyApi.Models {
     public class Pharmacy {
-    }
+		public int Id { get; set; }     //Поле идентифкатора
+		public string Login { get; set; }       //Поле логина
+		public string Password { get; set; }    //Поле пароля
+		public string PharmacyName { get; set; }    //Поле наименования
+		public double Latitude { get; set; }        //Поле широты(в градусах)
+		public double Longitude { get; set; }       //Поле долготы(в градусах)
+		public City City { get; set; }
+		public int CityId { get; set; }
+	}
 }
