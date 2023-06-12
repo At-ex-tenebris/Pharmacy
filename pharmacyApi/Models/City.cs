983af71279aceaefd20ac7f487ac5434b3d14d48
Ga1aktion <niksereev200231@gmail.com>
2023-06-12 20:54:01 +0300

    Была добавлена модель города(City)

--- pharmacyApi/Models/City.cs
+++ pharmacyApi/Models/City.cs
@@ -1,4 +1,11 @@
 ﻿namespace pharmacyApi.Models {
-    public class City {
+    public class City
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string CityName { get; set; }
+        public Region Region { get; set; }
+        public int RegionId { get; set; }
     }
 }
