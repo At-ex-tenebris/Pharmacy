b8680825ed84a3998feb928c707de1b53dfd691d
Ruslan <gimrainor10@mail.ru>
2023-06-12 21:35:21 +0300

    create medicament model

--- pharmacyApi/Models/Medicament.cs
+++ pharmacyApi/Models/Medicament.cs
@@ -1,4 +1,11 @@
 ﻿namespace pharmacyApi.Models {
     public class Medicament {
+        public int Id { get; set; }             // Поле идентификатора
+        public string Name { get; set; }        // Поле наименования
+        public string Description { get; set; } // Поле описания
+        public int Count { get; set; }          // Поле количества
+
+        public Pharmacy Pharmacy { get; set; }  // Поле связи с аптекой
+        public int PharmacyId { get; set; }
     }
 }
