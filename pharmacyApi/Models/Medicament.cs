24e50dbad580de3d50ebe88c94ffdd4adae47dd4
Ruslan <gimrainor10@mail.ru>
2023-06-12 21:01:22 +0300

    medicament api

--- pharmacyApi/Models/Medicament.cs
+++ pharmacyApi/Models/Medicament.cs
@@ -1,4 +1,11 @@
 ﻿namespace pharmacyApi.Models {
     public class Medicament {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Count { get; set; }
+
+        public Pharmacy Pharmacy { get; set; }
+        public int PharmacyId { get; set; }
     }
 }
