87b3b143a466545b72ff741cb7133e5ecbcdb241
Andrei <ankvsnkv@gmail.com>
2023-06-12 20:54:02 +0300

    Было создана модель Country и Administrator

--- pharmacyApi/Models/Administrator.cs
+++ pharmacyApi/Models/Administrator.cs
@@ -1,5 +1,9 @@
 ﻿namespace pharmacyApi.Models {
     public class Administrator {
 
+        public int Id { get; set; } //  Поле идентифкатора
+        public string Login { get; set; } // Поле логина
+        public string Password { get; set; } // Поле пароля
+
     }
 }
