39dd694a89bd370fd43f1dd898e7f7b8c78f555d
atextenebris <kurilenkonikitta@gmail.com>
2023-06-12 21:45:43 +0300

    Initial commit

---
+++ pharmacyApi/Utils/PasswordHasher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace pharmacyApi.Utils {
+    public static class PasswordHasher {
+        public static string Hash(string passwordStr){
+            var password = System.Text.Encoding.UTF8.GetBytes(passwordStr);
+            var hash = SHA512.HashData(password);
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
