1f7967d49528240bda698b766184fe68b914fab1
Sergula <post.gargalik@gmail.com>
2023-06-15 20:07:32 +0300

    Создал для контролер для аптеки

--- pharmacyApi/Models/Pharmacy.cs
+++ pharmacyApi/Models/Pharmacy.cs
@@ -1,12 +1,28 @@
-﻿namespace pharmacyApi.Models {
+﻿using System.Text.Json.Serialization;
+
+namespace pharmacyApi.Models {
     public class Pharmacy {
 		public int Id { get; set; }     //Поле идентифкатора
+		[JsonIgnore]
 		public string Login { get; set; }       //Поле логина
+		[JsonIgnore]
 		public string Password { get; set; }    //Поле пароля
 		public string PharmacyName { get; set; }    //Поле наименования
 		public double Latitude { get; set; }        //Поле широты(в градусах)
 		public double Longitude { get; set; }       //Поле долготы(в градусах)
 		public City City { get; set; }
 		public int CityId { get; set; }
-	}
+        public static Pharmacy FromFull(FullPharmacy obj)
+        {
+            return new Pharmacy { 
+				Id = obj.Id, 
+				Login = obj.Login,
+				Password = obj.Password,
+				PharmacyName = obj.PharmacyName, 
+				Latitude = obj.Latitude, 
+				Longitude = obj.Longitude,
+				City = obj.City,
+				CityId = obj.CityId};
+        }
+    }
 }
