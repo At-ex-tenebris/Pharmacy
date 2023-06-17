827eb01c03f599a74567c80382eb02162e626a6b
atextenebris <kurilenkonikitta@gmail.com>
2023-06-17 14:48:50 +0300

    Сформирована модель области

--- src/app/shared/models/region.model.ts
+++ src/app/shared/models/region.model.ts
@@ -1,3 +1,25 @@
-export class Region{
-    
+import { Country } from './country.model';
+
+export class Region {
+  id: number;
+  login: string;
+  password: string;
+  name: string;
+  country: Country | null;
+  countryId: number;
+  constructor(
+    id: number,
+    login: string,
+    password: string,
+    name: string,
+    countryId: number,
+    country: Country | null
+  ) {
+    this.id = id;
+    this.login = login;
+    this.password = password;
+    this.name = name;
+    this.country = country;
+    this.countryId = countryId;
+  }
 }
