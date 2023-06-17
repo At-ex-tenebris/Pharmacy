c8fbffe5c11b25a40ba676f4129fbbe64b3b2a26
Nikita <niksereev200231@gmail.com>
2023-06-17 14:49:47 +0300

    Добавил модель города(city)

--- src/app/shared/models/city.model.ts
+++ src/app/shared/models/city.model.ts
@@ -1,3 +1,17 @@
+import { Region } from "./region.model";
 export class City{
-    
+  id: number;
+  login: string;
+  password: string;
+  cityName: string;
+  region: Region | null;
+  regionId: number;
+  constructor(Id: number, login: string, password: string, cityName: string, regionId: number, region: Region | null) {
+    this.id = Id;
+    this.login = login;
+    this.password = password;
+    this.cityName = cityName;
+    this.regionId = regionId;
+    this.region = region;
+  }
 }
