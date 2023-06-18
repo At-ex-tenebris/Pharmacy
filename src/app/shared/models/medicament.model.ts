957a05bdd7576b6196e2ebd5d1ac1674a9abdeea
Ruslan <gimrainor10@mail.ru>
2023-06-18 21:43:26 +0300

    Создана модель ангуляра медикаментов

--- src/app/shared/models/medicament.model.ts
+++ src/app/shared/models/medicament.model.ts
@@ -1,3 +1,20 @@
-export class Medicament{
-    
+import { Pharmacy } from "./pharmacy.model";
+export class Medicament {
+    Id: number;
+    Name: string;
+    Description: string;
+    Count: number;
+    Pharmacy: Pharmacy | null;
+    PharmacyId: number;
+
+    constructor(Id: number, Name: string, Description: string,
+        Count: number, Pharmacy: Pharmacy | null, PharmacyId: number,)
+    {
+        this.Id = Id;
+        this.Name = Name;
+        this.Description = Description;
+        this.Count = Count;
+        this.Pharmacy = Pharmacy;
+        this.PharmacyId = PharmacyId;
+    }
 }
